875a8e4245d2f7e7a6441b55337c6ae5b25f6449
Letícia Neri <letheskrodderi@gmail.com>
2020-03-18 09:51:47 -0300

    Create 1006.c

---
+++ uri/1006.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+ 
+int main() {
+ float A, B, C, MEDIA;
+ scanf("%f",&A);
+ scanf("%f",&B);
+ scanf("%f",&C);
+ MEDIA = (2*A  +3*B +5*C)/10;
+ printf("MEDIA = %.1f",MEDIA);
+ printf("\n");
+    return 0;
+}
