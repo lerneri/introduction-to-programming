4cfba88e63b0430d362eef08224e43601e65e8f4
Letícia Neri <letheskrodderi@gmail.com>
2020-03-18 10:08:29 -0300

    Create 1014.c

---
+++ uri/1014.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    int X;
+    float l, Y;
+    scanf("%d",&X);
+    scanf("%f",&Y);
+    l = X/Y;
+    printf("%.3f km/l",l);
+    printf("\n");
+ 
+    return 0;
+}
