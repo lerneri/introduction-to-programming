f720956de9f079bfa1db1d43a9bf29033c0fec52
Letícia Neri <letheskrodderi@gmail.com>
2020-03-20 11:53:03 -0300

    Update item04.c

--- fup/lista4/item04.c
+++ fup/lista4/item04.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 int main(void) {
-int i, n;
-//n<=1023
-double x;
-x=2;
-scanf("%d",&n);
-for(i=1; i<=n; i++){
+ int i, n;
+ //n<=1023
+ double x;
+ x=2;
+ scanf("%d",&n);
+ for(i=1; i<=n; i++){
   printf("2^%d=%.0lf\n",i,x);
   x=2*x;
-}
-
+ }
+ return 0;
 }
