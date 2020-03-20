2d2e0dbd462db15412535420d0d559322da26755
Letícia Neri <letheskrodderi@gmail.com>
2020-03-20 10:15:16 -0300

    Update item3.c

--- fup/lista4/item3.c
+++ fup/lista4/item3.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 int main (){
-  int N, i;
+  int i;
   // N<=0
-  double s, avg;
+  double N, s, avg;
   printf("Digite um número inteiro positivo: ");
-  scanf("%d",&N);
+  scanf("%lf",&N);
   for (s=0, i=1; i<=N; i++){
     s = s+i;
   }
   avg=s/N;
-  printf("Média da soma dos %d primeiros inteiros positivos = %.1lf",N,avg);
+  printf("Média da soma dos %.0lf primeiros inteiros positivos = %.1lf",N,avg);
 }
