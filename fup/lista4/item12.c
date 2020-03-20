b694699690e45ea44d3bb30dfacec91d2e4d97dc
Letícia Neri <letheskrodderi@gmail.com>
2020-03-20 12:31:57 -0300

    Create item12.c

---
+++ fup/lista4/item12.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void) {
+ int NUM, i;
+ //Vamos mostrar os múltiplos de 3 e os múltiplos de 5
+ scanf("%d",&NUM);
+
+ for(i=1; i<=NUM; i++){
+  
+  if (i%3==0){
+    printf("%d é múltiplo de 3\n", i);
+  }
+ 
+  if (i%5==0){
+    printf("%d é múltiplo de 5\n", i);
+  }
+  
+ }
+
+ return 0;
+}
