be66e07a0c90637aff47fdbe8f0095da3142bedf
Letícia Neri <letheskrodderi@gmail.com>
2020-03-22 18:30:25 -0300

    Create item14.c

---
+++ fup/lista4/item14.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+ int A, B, s, i;
+ scanf("%d",&A);
+ scanf("%d",&B);
+ s=0;
+ for(i=1;i<=B;i++){
+   s=s+A;
+   }
+ printf("%d",s);
+ return 0; 
+}
