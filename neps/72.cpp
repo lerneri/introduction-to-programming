32a9c87b74b33227592ece02bc0ca3305e568bdf
Letícia Neri <19753752+lerneri@users.noreply.github.com>
2020-04-14 12:39:07 -0300

    Update and rename cobracoral.c to 72.cpp

--- neps/72.cpp
+++ neps/72.cpp
@@ -1,15 +1,15 @@
-#include <stdio.h>
+#include <iostream>
 
 int main(void) {
   int w, x, y, z;
-  scanf("%d %d %d %d",&w,&x,&y,&z);
+  cin >> w >> x >> y >> z;
   if (w>=1 && w<=9 && x>=1 && x<=9 && y>=1 && y<=9 && z>=1 && z<=9){
    if(w==y || x==z){
-     printf("V\n");
+     cout << "V\n";
      return 0;
    }
    if(w==z){
-     printf("F\n");
+     cout << "F\n";
    }
   }
   return 0;
