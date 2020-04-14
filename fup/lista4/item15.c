2772eec25aa9e3d24d4114e78e5b56b4b0fa5208
Letícia Neri <19753752+lerneri@users.noreply.github.com>
2020-04-14 12:49:33 -0300

    Update item15.c

--- fup/lista4/item15.c
+++ fup/lista4/item15.c
@@ -1,23 +1,6 @@
 //Letícia Rodrigues Neri
-//496518
 //Engenharia de Computação - UFC
 //FUP - Lista 4 - item 15
-
-/*ALGORITMO
-
-	DECLARE A, B, p, i NUMERICO
-	LEIA A
-	LEIA B
-	p🡐1
-	PARA i=1 ATÉ i =B FAÇA
-	INÍCIO
-		p🡐p*A
-
-	FIM
-	ESCREVA p
-	
-FIM_ALGORITMO.*/
-
 
 #include <stdio.h>
 
