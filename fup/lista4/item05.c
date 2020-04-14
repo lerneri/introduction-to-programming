573907cee284e39291ec50d1b5f9f904a5c571fa
Letícia Neri <19753752+lerneri@users.noreply.github.com>
2020-04-14 12:44:52 -0300

    Add files via upload

---
+++ fup/lista4/item05.c
@@ -0,0 +1,38 @@
+//Letícia Rodrigues Neri
+//496518
+//Engenharia de Computação - UFC
+
+/*ALGORITMO
+	DECLARE n, m NUMERICO
+
+	LEIA m
+
+	ENQUANTO n!=0 FAÇA
+	INÍCIO
+		LEIA n
+
+		SE n>m ENTAO
+		INÍCIO
+			m🡐n
+
+		FIM
+	FIM
+	ESCREVA O maior número é m
+FIM_ALGORITMO.*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+ double n, m;
+ 
+ scanf("%lf",&m);
+ while(n!=0){//Inserir 0 para parar as entradas
+   scanf("%lf",&n);
+   if(n>m){
+     m=n;
+   }
+   }
+ printf("O maior número é %lf",m);
+ return 0; 
+}
