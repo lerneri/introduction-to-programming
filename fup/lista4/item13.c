a20ac7fc1c5722ddee878578970a1852e3efe26e
Letícia Neri <19753752+lerneri@users.noreply.github.com>
2020-04-14 12:49:08 -0300

    Update item13.c

--- fup/lista4/item13.c
+++ fup/lista4/item13.c
@@ -1,34 +1,7 @@
 //Letícia Rodrigues Neri
-//496518
+
 //Engenharia de Computação - UFC
 //FUP - Lista 4 - item 13
-
-/*ALGORITMO
-
-	DECLARE n, x, i, par=0, impar=0 NUMERICO
-
-	LEIA n
-
-
-	PARA  i=1 ATÉ  i=n FAÇA
-	INÍCIO
-		LEIA x
-
-		SE x%2 = 0 ENTAO
-		INÍCIO
-			par🡐par+1
-
-		FIM
-		SENÃO
- 		INÍCIO
-			impar🡐impar+1
-
-		FIM
-	FIM
-	ESCREVA Você digitou par números pares e impar números ímpares
-
-
-FIM_ALGORITMO.*/
 
 #include <stdio.h>
 
